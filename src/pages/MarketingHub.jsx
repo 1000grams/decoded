c027c69e515054ca99ce1d046013e08c6f61a6da
1000grams <AHprivateEMAIL@protonmail.com>
2025-06-15 16:43:29 -0400

    Add marketing hub with attribution and automation

---
+++ src/pages/MarketingHub.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MarketingHub() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Marketing Hub</h1>
+      <ul>
+        <li>Ad Spend Summary</li>
+        <li>ROI & Attribution</li>
+        <li>Post Scheduler</li>
+        <li>Trending-Based Auto-Reposts</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MarketingHub;
