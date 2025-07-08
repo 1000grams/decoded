e634ed8d2c605c52df7a9b6595275a61e4e0908e
1000grams <AHprivateEMAIL@protonmail.com>
2025-07-08 01:41:08 -0400

    landing page and color styles 7-7-25

--- test-api.js
+++ test-api.js
@@ -2,7 +2,7 @@ import https from 'https';
 
 const options = {
     hostname: '2h2oj7u446.execute-api.eu-central-1.amazonaws.com',
-    path: '/prod/catalog',
+    path: '/dashboard/catalog', // Corrected path
     method: 'GET',
     headers: {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' // Replace with a valid token
